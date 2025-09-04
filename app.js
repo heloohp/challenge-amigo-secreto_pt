18eac06f9709f66f611b744d78238e9462ae1ad1
Heloisa Pereira <heloisa.pereira@g.globo>
2025-09-04 16:06:24 -0300

    feat: adicionar funcionalidade de sorteio de amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value;
+    if (nome === '') {
+        alert('Por favor, insira um nome.');
+    return;
+}
+    amigos.push(nome);
+    document.getElementById('amigo').value = '';
+
+    exibirAmigos();
+}
+
+function exibirAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    amigos.forEach(function(amigo) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Adicione pelo menos um amigo antes de sortear.');
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    document.getElementById('resultado').innerHTML = 'O amigo secreto sorteado é: ' + amigoSorteado;
+    amigos = [];
+    document.getElementById('listaAmigos').innerHTML = '';
+}
